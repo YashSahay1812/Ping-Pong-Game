808937ba6ddc8c195d239b1d0b9d7e32e6d8a592
unknown <yashsahay999@gmail.com>
2022-05-10 22:50:15 +0530

    Added control to paddles and motion to balls

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 let ball = {
     x : canvas.width / 2,
     y : canvas.height / 2,
-    radius : 10
+    radius : 10,
+    dx : 7,
+    dy : 5
 }
 class Paddle {
     constructor(x,y){
@@ -13,6 +13,7 @@ class Paddle {
         this.y = y;
         this.width = 20;
         this.height = 100;
+        this.speed = 30;
     }
 }
 
@@ -20,21 +21,75 @@ let p1 = new Paddle(5,(canvas.height/2) - 50);
 let p2 = new Paddle(canvas.width - 25, (canvas.height/2) - 50);
 
 // DRAWING PADDLES AND BALL
-ctx.fillStyle = "blue";
-ctx.fillRect(p1.x,p1.y,p1.width,p1.height);
-ctx.fillRect(p2.x,p2.y,p2.width,p2.height);
-ctx.beginPath();
-ctx.arc(ball.x,ball.y,ball.radius,0,Math.PI*2,false);
-ctx.fillStyle = "red";
-ctx.fill();
+function drawP1(){
+    ctx.fillStyle = "blue";
+    ctx.fillRect(p1.x,p1.y,p1.width,p1.height);
+}
+function drawP2(){
+    ctx.fillStyle = "blue";
+    ctx.fillRect(p2.x,p2.y,p2.width,p2.height);
+}
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(ball.x,ball.y,ball.radius,0,Math.PI*2,false);
+    ctx.fillStyle = "red";
+    ctx.fill();
+}
+drawP1();
+drawP2();
+drawBall();
 
 // CREATING SCORE
+createLeftScore();
+createRightScore();
 // Left Score
-ctx.font = '50px Arial';
-ctx.fillStyle = "black";
-ctx.fillText("0",canvas.width/4,canvas.height/5);
+function createLeftScore(){
+    ctx.font = '50px Arial';
+    ctx.fillStyle = "black";
+    ctx.fillText("0",canvas.width/4,canvas.height/5);
+}
 
 // Right Score
-ctx.font = '50px Arial';
-ctx.fillStyle = "black";
-ctx.fillText("0",canvas.width - canvas.width/4,canvas.height/5);
+function createRightScore(){
+    ctx.font = '50px Arial';
+    ctx.fillStyle = "black";
+    ctx.fillText("0",canvas.width - canvas.width/4,canvas.height/5);
+}
+
+document.addEventListener('keydown',function(event){
+    // FOR LEFT PLAYER
+    if(event.key === 'w'){
+        if(p1.y >= 5) p1.y -= p1.speed;
+    }
+    if(event.key === 's'){
+        if(p1.y <= canvas.height-p1.height-5) p1.y += p1.speed;
+    }
+    // FOR RIGHT PLAYER
+    if(event.key === 'ArrowUp'){
+        if(p2.y >= 5) p2.y -= p2.speed;
+    }
+    if(event.key === 'ArrowDown'){
+        if(p2.y <= canvas.height-p2.height-5) p2.y += p2.speed;
+    }
+})
+
+function playGame(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    if(ball.x <= ball.radius || ball.x >= canvas.width - ball.radius){
+        ball.dx = -ball.dx;
+    }
+    if(ball.y <= ball.radius || ball.y >= canvas.height - ball.radius){
+        ball.dy = -ball.dy;
+    }
+
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    drawBall();
+    drawP1();
+    drawP2();
+    createLeftScore();
+    createRightScore();
+    requestAnimationFrame(playGame);
+}
+playGame();
